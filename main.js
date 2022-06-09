7c862914e99f2e2d976a11b39b548d2069fef278
Mario <mc.fialho@hotmail.com>
2022-06-09 09:26:03 -0300

    update

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
-/*
+
 const html = document.querySelector('html');
 const checkbox = document.querySelector('#checkbox');
 
 checkbox.addEventListener('change', e => {    
     html.classList.toggle('dark-mode')
 })
-*/
+
 
 const img1 = document.getElementById('img1')
 const img2 = document.getElementById('img2')
 const img3 = document.getElementById('img3')
 
-console.log(img1)
 let counter = 0;
 
 function changeImg() {
